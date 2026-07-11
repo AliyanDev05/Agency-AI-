a9693c7d822b91cfd69a46b07defe610f5df9683
AliyanDev05 <aliyanabbas2812@gmail.com>
2026-07-11 18:24:28 +0500

    [AgencyAI-0002] Completed Hero Section with gradient image and text

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   const [theme, setTheme] = useState(() => {
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   );
 }
