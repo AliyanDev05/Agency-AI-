a65633826b8075ce34d705f170d11ac056329701
AliyanDev05 <aliyanabbas2812@gmail.com>
2026-07-11 18:48:42 +0500

    [AgencyAI-0003] Develop the TrustedBy companies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
 
 function App() {
   const [theme, setTheme] = useState(() => {
@@ -14,6 +15,7 @@ function App() {
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
+      <TrustedBy />
     </div>
   );
 }
